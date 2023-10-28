544076787fd843add1e0bfe7ef363f13ab880d6e
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-28 10:15:29 +0530

    Added Palindrome Index

---
+++ Palindrome Index/Palindrome Index.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+int palindromeIndex(char* s)
+{
+    int i, j, l = strlen(s), a, b;
+    for(i = 0, j = l - 1; i < l; i++, j--)
+        if(*(s+i)!=*(s+j))
+            break;
+    if(i>j)
+        return -1;
+    for(a = i + 1, b = j; a < j && b > i + 1; a++, b--)
+        if(*(s+a)!=*(s+b))
+            return j;
+    return i;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* q_endptr;
+    char* q_str = readline();
+    int q = strtol(q_str, &q_endptr, 10);
+
+    if (q_endptr == q_str || *q_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        char* s = readline();
+
+        int result = palindromeIndex(s);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
